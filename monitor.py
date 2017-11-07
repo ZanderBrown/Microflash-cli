0ffe66f82ae86f8454f38a044c429595163e4ec0
Zander Brown <ZanderBrown@users.noreply.github.com>
2017-11-07 23:39:30 +0000

    Emulate copy_async with threading.Thread
    Use copy_async where possible but fallback to a threading.Thread wrapper around copy
    
    Now complies with PEP8

--- monitor.py
+++ monitor.py
@@ -1,34 +1,55 @@
 #! /usr/bin/env python3
 
 from gi.repository import GLib, Gio
-from os.path import getsize
 
 try:
-	file = Gio.File.new_for_path ("{}/Downloads".format(GLib.get_home_dir ()));
-	monitor = file.monitor (Gio.FileMonitorFlags.NONE, None);
-	print ("Monitoring: {}".format(file.get_path ()));
+    wdir = "{}/Downloads".format(GLib.get_home_dir())
+    gfile = Gio.File.new_for_path(wdir)
 
-	def cb (obj, src, dest, event):
-		if event == Gio.FileMonitorEvent.CREATED and src.get_path().endswith(".hex"):
-			# Ideally we check if the hex is valid somehow
-			if getsize(src.get_path ()) < 1:
-				return
-			print ("Found: {}".format(src.get_path ()));
-			drives = Gio.VolumeMonitor.get()
-			for drive in drives.get_volumes():
-				if drive.get_name() == "MICROBIT":
-					location = drive.get_mount().get_default_location()
-					try:
-						dest = Gio.File.new_for_path("{}/firmware.hex".format(location.get_path()))
-						def done (src, res, user_data):
-							src.copy_finish (res)
-							print("Flashed: {}".format(user_data))
-						src.copy_async(dest, Gio.FileCopyFlags.OVERWRITE, GLib.PRIORITY_DEFAULT, None, None, None, done, location.get_path())
-					except:
-						print("Failed to copy to {}".format(location.get_path()))
+    polyfil = "copy_async" not in dir(gfile)
+    if polyfil:
+        import threading
 
-	monitor.connect ("changed", cb);
+    monitor = gfile.monitor(Gio.FileMonitorFlags.NONE, None)
+    print("Monitoring: {}".format(gfile.get_path()))
 
-	GLib.MainLoop().run() 
-except KeyboardInterrupt: 
-	GLib.MainLoop().quit() 
+    def cb(obj, src, dest, event):
+        if (event == Gio.FileMonitorEvent.CREATED and
+                src.get_path().endswith(".hex")):
+            # Ideally we check if the hex is valid somehow
+            print("Found: {}".format(src.get_path()))
+            drives = Gio.VolumeMonitor.get()
+            for drive in drives.get_volumes():
+                if drive.get_name() == "MICROBIT":
+                    location = drive.get_mount().get_default_location()
+                    try:
+                        mb = location.get_path()
+
+                        path = "{}/firmware.hex".format(mb)
+                        dest = Gio.File.new_for_path(path)
+
+                        flags = Gio.FileCopyFlags.OVERWRITE
+                        priority = GLib.PRIORITY_DEFAULT
+
+                        def done(src, res, user_data):
+                            src.copy_finish(res)
+                            print("Flashed: {}".format(user_data))
+
+                        if polyfil:
+                            def wrap():
+                                src.copy(dest, flags)
+                                print("Flashed: {}".format(mb))
+
+                            threading.Thread(target=wrap).start()
+                        else:
+                            src.copy_async(dest, flags, priority,
+                                           None, None, None, done, mb)
+
+                    except:
+                        print("Failed to copy to {}".format(mb))
+
+    monitor.connect("changed", cb)
+
+    GLib.MainLoop().run()
+except KeyboardInterrupt:
+    GLib.MainLoop().quit()
