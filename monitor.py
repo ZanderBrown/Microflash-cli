fe9726b440af7b13182b2ade01d97f1e0cbbc55f
Zander Brown <ZanderBrown@users.noreply.github.com>
2018-01-19 11:14:56 +0000

    Refactor into a class, various tweaks
    Chromium compatibility is still hit and miss but seems
    to work better with the copy_async polyfil than copy_async
    itself, on the plus side that should work with the default
    Raspbian setup

--- monitor.py
+++ monitor.py
@@ -3,133 +3,142 @@
 from gi.repository import GLib, Gio
 from os.path import getsize
 
+# 'copy_async' is unavailable in some releases of the Gio bindings
+# so we import Thread so that we may emulate 'copy_async' if needed
+from threading import Thread
+
+class Microflash:
+    def __init__(self, path):
+        # Setup a GFile for the directory we are interested in
+        gfile = Gio.File.new_for_path(path)
+
+        # Fetch a GFileMonitor for the interesting directory
+        self.monitor = gfile.monitor(Gio.FileMonitorFlags.NONE, None)
+
+        self.operations = {}
+        # We should attempt to override existing
+        self.flags = Gio.FileCopyFlags.OVERWRITE
+        # The operation is of standard priority
+        self.priority = GLib.PRIORITY_DEFAULT
+
+        # If the gfile instance of GFile doesn't have a member called
+        # 'copy_async' we should emulate it
+        self.polyfil = "copy_async" not in dir(gfile)
+        
+        # Connect the callback to the changed signal
+        self.monitor.connect("changed", self.cb)
+
+        # State where we are monitoring
+        print("Monitoring: {}".format(gfile.get_path()))
+
+    # Callback for copy_async so we can
+    # communicate the fact it's finished
+    def copy_async(self, src, res, mb):
+        try:
+            # Ensure the write is compleate
+            src.copy_finish(res)
+            # Say where we copied to
+            print("Flashed: {}".format(mb))
+            self.operations[mb] = None
+        except Exception as e:
+            self.operations[mb] = None
+            print("Bad stuff", str(e))
+
+    # A wrapper around 'copy' so we can use it
+    # as the target of a threading.Thread
+    def copy(self, src, dest, mb):
+        # Copy the file 'src' to the destination
+        # 'dest' with flags
+        try:
+            src.copy(dest, self.flags, self.operations[mb])
+            # Copy is blocking so this will be run
+            # when it has successfully compleated
+            # Show that we managed to flash the hex
+            print("Flashed: {}".format(mb))
+            self.operations[mb] = None
+        except Exception as e:
+            self.operations[mb] = None
+            print("Bad stuff", str(e))
+
+    # This function is run whenever 'monitor' notices a change
+    def cb(self, obj, src, dest, event):
+        # If it was a create event and the file extension is hex
+        if (event == Gio.FileMonitorEvent.CREATED and
+                src.get_path().endswith(".hex")):
+            # Ideally we check if the hex is valid somehow as
+            # all this does it check it isn't an empty file
+            if getsize(src.get_path()) < 1:
+                return
+            # Say we found a hex
+            print("Found: {}".format(src.get_path()))
+
+            # Get a refrence to the default GVolumeMonitor that allows
+            # us to inspect the connected Volumes
+            drives = Gio.VolumeMonitor.get()
+            # Intereate over all the current volumes
+            for drive in drives.get_volumes():
+                # If this volume is called MICROBIT we assume it's a
+                # micro:bit rather than HDD ect
+                if drive.get_name() == "MICROBIT":
+                    # Find out where the micro:bit was mounted to
+                    mount = drive.get_mount()
+                    if not mount:
+                        if not mount.can_mount():
+                            print("Unable to mount micro:bit")
+                            continue
+                        # todo: mount
+                        print('Bad')
+                        continue
+                    location = mount.get_default_location()
+                    # copy / copy_async may fail so here we catch
+                    # exceptions with the hope it may succeed on
+                    # another connected micro:bit
+                    try:
+                        # get the mount location as a string
+                        mb = location.get_path()
+
+                        if mb in self.operations and self.operations[mb]:
+                            print("Cancelling current operation")
+                            self.operations[mb].cancel()
+
+                        self.operations[mb] = Gio.Cancellable()
+
+                        # we must copy to a file dispite the micro:bits
+                        # virtual fs system
+                        path = "{}/firmware.hex".format(mb)
+                        # a GFile for the destination path
+                        dest = Gio.File.new_for_path(path)
+
+                        # If copy_async is unavailable we have to copy
+                        # the hex differently to emulate it
+                        if self.polyfil:
+                            # Run 'wrap' in a thread so that we may
+                            # flash multiple bits simultaneously
+                            Thread(target=self.copy,
+                                args=(src, dest, mb)).start()
+                        else:
+                            # Copy 'src' to 'dest' asynchronous with
+                            # the givien flags and priority. We also
+                            # pass three None because we are not
+                            # providing a GCancellable or a progress
+                            # callback (at least presently) then we
+                            # provide the 'done' callback and some
+                            # data to pass to it so it know who called
+                            # back so it can communicate compleation
+                            src.copy_async(dest, self.flags,
+                                           self.priority,
+                                           self.operations[mb], None,
+                                           None, self.copy_async, mb)
+
+                    except Exception as e:
+                        # Something broke, say we couldn't copy to
+                        # this micro:bit
+                        print("Failed to copy to {}\nError: {}".format(mb, str(e)))
+
 # Directory to monitor,
 # by default we use the current users downloads
 wdir = "{}/Downloads".format(GLib.get_home_dir())
-# Setup a GFile for the directory we are interested in
-gfile = Gio.File.new_for_path(wdir)
-
-# If the gfile instance of GFile doesn't have a member called
-# 'copy_async' we should emulate it
-polyfil = "copy_async" not in dir(gfile)
-if polyfil:
-    # We import threading here to avoid importing it multiple
-    # times later when we are copying files, however it isn't
-    # imported at the top of the script because we only need it
-    # when 'copy_async' is unavailable
-    import threading
-
-# Fetch a GFileMonitor for the interesting directory
-monitor = gfile.monitor(Gio.FileMonitorFlags.NONE, None)
-# State where we are monitoring
-print("Monitoring: {}".format(gfile.get_path()))
-
-operations = {}
-
-# This function is run whenever 'monitor' notices a change
-def cb(obj, src, dest, event):
-    global operations
-    # If it was a create event and the file extension is hex
-    if (event == Gio.FileMonitorEvent.CREATED and
-            src.get_path().endswith(".hex")):
-        # Ideally we check if the hex is valid somehow as
-        # all this does it check it isn't an empty file
-        if getsize(src.get_path()) < 1:
-            return
-        # Say we found a hex
-        print("Found: {}".format(src.get_path()))
-
-        # Get a refrence to the default GVolumeMonitor that allows
-        # us to inspect the connected Volumes
-        drives = Gio.VolumeMonitor.get()
-        # Intereate over all the current volumes
-        for drive in drives.get_volumes():
-            # If this volume is called MICROBIT we assume it's a
-            # micro:bit rather than HDD ect
-            if drive.get_name() == "MICROBIT":
-                # Find out where the micro:bit was mounted to
-                location = drive.get_mount().get_default_location()
-                # copy / copy_async may fail so here we catch
-                # exceptions with the hope it may succeed on
-                # another connected micro:bit
-                try:
-                    # get the mount location as a string
-                    mb = location.get_path()
-
-                    if mb in operations and operations[mb]:
-                        print("Cancelling current operation")
-                        operations[mb].cancel()
-
-                    operations[mb] = Gio.Cancellable()
-
-                    # we must copy to a file dispite the micro:bits
-                    # virtual fs system
-                    path = "{}/firmware.hex".format(mb)
-                    # a GFile for the destination path
-                    dest = Gio.File.new_for_path(path)
-
-                    # We should attempt to override existing
-                    flags = Gio.FileCopyFlags.OVERWRITE
-                    # The operation is of standard priority
-                    priority = GLib.PRIORITY_DEFAULT
-
-                    # If copy_async is unavailable we have to copy
-                    # the hex differently to emulate it
-                    if polyfil:
-                        # A wrapper around 'copy' so we can use it
-                        # as the target of a threading.Thread
-                        def wrap():
-                            global operations
-                            # Copy the file 'src' to the destination
-                            # 'dest' with flags
-                            try:
-                                src.copy(dest, flags, operations[mb])
-                                # Copy is blocking so this will be run
-                                # when it has successfully compleated
-                                # Show that we managed to flash the hex
-                                print("Flashed: {}".format(mb))
-                                operations[mb] = None
-                            except Exception as e:
-                                operations[mb] = None
-                                print("Bad stuff", str(e))
-
-                        # Run 'wrap' in a thread so that we may
-                        # flash multiple bits simultaneously
-                        threading.Thread(target=wrap).start()
-                    else:
-                        # Callback for copy_async so we can
-                        # communicate the fact it's finished
-                        def done(src, res, user_data):
-                            global operations
-                            try:
-                                # Ensure the write is compleate
-                                src.copy_finish(res)
-                                # Say where we copied to
-                                print("Flashed: {}".format(user_data))
-                                operations[mb] = None
-                            except Exception as e:
-                                operations[mb] = None
-                                print("Bad stuff", str(e))
-
-                        # Copy 'src' to 'dest' asynchronous with
-                        # the givien flags and priority. We also
-                        # pass three None because we are not
-                        # providing a GCancellable or a progress
-                        # callback (at least presently) then we
-                        # provide the 'done' callback and some
-                        # data to pass to it so it know who called
-                        # back so it can communicate compleation
-                        src.copy_async(dest, flags, priority,
-                                       operations[mb], None, None,
-                                       done, mb)
-
-                except:
-                    # Something broke, say we couldn't copy to
-                    # this micro:bit
-                    print("Failed to copy to {}".format(mb))
-
-# Connect the callback to the changed signal
-monitor.connect("changed", cb)
+mf = Microflash(wdir)
 
 # This try is used to catch events such as Ctrl-C
 # so we can exit gracefully
